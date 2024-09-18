3e0ce87cbabbaccdf7f96c5e5a3c9a7c6ffecbdc
Mohamed Irfaan I <mohamedirfaan.i@datayaan.com>
2024-09-18 16:21:15 +0530

    [ADD] Added bootstrap vue and vuetify js file.

---
+++ front-end-project/src/plugins/bootStrap.js
@@ -0,0 +1,57 @@
+import Vue from "vue";
+// import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+
+import {
+  BootstrapVue,
+  IconsPlugin,
+  LayoutPlugin,
+  CardPlugin,
+  VBScrollspyPlugin,
+  DropdownPlugin,
+  ModalPlugin,
+  TablePlugin,
+  ButtonPlugin,
+  OverlayPlugin,
+  FormPlugin,
+  FormSelectPlugin,
+  FormCheckboxPlugin,
+  FormGroupPlugin,
+  FormInputPlugin,
+  FormRadioPlugin,
+  FormTextareaPlugin,
+  LinkPlugin,
+  NavbarPlugin,
+  TooltipPlugin,
+  VBTooltipPlugin,
+  VBPopoverPlugin,
+  VBHoverPlugin,
+  PaginationPlugin,
+  TabsPlugin,
+} from "bootstrap-vue";
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(LayoutPlugin);
+Vue.use(ModalPlugin);
+Vue.use(CardPlugin);
+Vue.use(VBScrollspyPlugin);
+Vue.use(DropdownPlugin);
+Vue.use(TablePlugin);
+Vue.use(ButtonPlugin);
+Vue.use(CardPlugin);
+Vue.use(FormPlugin);
+Vue.use(FormCheckboxPlugin);
+Vue.use(FormGroupPlugin);
+Vue.use(FormInputPlugin);
+Vue.use(FormRadioPlugin);
+Vue.use(FormSelectPlugin);
+Vue.use(LinkPlugin);
+Vue.use(NavbarPlugin);
+Vue.use(OverlayPlugin);
+Vue.use(TooltipPlugin);
+Vue.use(FormTextareaPlugin);
+Vue.use(VBTooltipPlugin);
+Vue.use(VBPopoverPlugin);
+Vue.use(VBHoverPlugin);
+Vue.use(PaginationPlugin);
+Vue.use(TabsPlugin);
